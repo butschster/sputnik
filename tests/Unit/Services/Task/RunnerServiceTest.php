c49dd092a1bf3d66a0ed1fadf3a6221510fd1589
Pavel Buchnev <butschster@gmail.com>
2019-08-07 19:26:01 +0300

    Add net test.

--- tests/Unit/Services/Task/RunnerServiceTest.php
+++ tests/Unit/Services/Task/RunnerServiceTest.php
@@ -9,8 +9,9 @@ use App\Utils\SSH\Contracts\ProcessExecutor;
 use App\Utils\SSH\ScriptsStorage;
 use App\Utils\SSH\Shell\Response;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\TestCase;
+use Illuminate\Support\Carbon;
 use Mockery as m;
+use Tests\TestCase;
 
 class ExecutorServiceTest extends TestCase
 {
@@ -18,22 +19,9 @@ class ExecutorServiceTest extends TestCase
 
     function test_a_task_can_be_run()
     {
-        $this->mock(KeyStorage::class, function ($mock) {
-            $mock->shouldReceive('storeKey');
-        });
-
-        $this->mock(ScriptsStorage::class, function ($mock) {
-            $mock->shouldReceive('storeScript')->once()->andReturn(base_path('tests/fixtures/Ssh/script'));
-        });
+        $service = $this->getExecutorService();
 
         $task = $this->createTask();
-
-        $executor = m::mock(ProcessExecutor::class);
-        $executor->shouldReceive('run')->andReturn(new Response(0, 'success'));
-
-        $service = new ExecutorService(
-            $executor
-        );
 
         $service->run($task);
         $task->refresh();
@@ -45,28 +33,14 @@ class ExecutorServiceTest extends TestCase
 
     function test_a_task_can_be_run_in_background()
     {
-        $this->mock(KeyStorage::class, function ($mock) {
-            $mock->shouldReceive('storeKey');
-        });
-
-        $this->mock(ScriptsStorage::class, function ($mock) {
-            $mock->shouldReceive('storeScript')->once()->andReturn(base_path('tests/fixtures/Ssh/script'));
-        });
-
+        $service = $this->getExecutorService();
         $task = $this->createTask();
-
-        $executor = m::mock(ProcessExecutor::class);
-        $executor->shouldReceive('run')->andReturn(new Response(0, 'success'));
-
-        $service = new ExecutorService(
-            $executor
-        );
 
         $service->runInBackground($task);
 
         $this->sendCallbackRequest('task.finished', [
             'exit_code' => 1,
-            'task_id' => $task->id
+            'task_id' => $task->id,
         ])->assertOk();
 
         $task->refresh();
@@ -74,4 +48,49 @@ class ExecutorServiceTest extends TestCase
         $this->assertEquals(1, $task->exit_code);
         $this->assertEquals(Task::STATUS_FINISHED, $task->status);
     }
+
+    function test_the_task_should_contains_callback_request_in_the_end()
+    {
+        Carbon::setTestNow('2010-10-10');
+
+        $task = $this->createTask([
+            'id' => 'abcd-efgh-ijkl-mnop'
+        ]);
+
+        $service = $this->getExecutorService(function($mock) use($task) {
+            $mock->shouldReceive('storeScript')->once()->andReturnUsing(function ($script) use($task) {
+
+                $this->assertStringContainsString(
+                    'curl -X POST -k -d "task_id=abcd-efgh-ijkl-mnop&exit_code=$STATUS&action=task.finished&expires=1286672400&signature=ab57abdd4ad8deb12b792a0938b5ec7840a32c7c6283ba2e2942d580efa2b611" http://localhost/callback > /dev/null 2>&1',
+                    $script
+                );
+
+                return '';
+            });
+        });
+
+        $service->run($task);
+    }
+
+    /**
+     * @param \Closure|null $scriptsStorageMock
+     *
+     * @return ExecutorService
+     */
+    protected function getExecutorService(\Closure $scriptsStorageMock = null): ExecutorService
+    {
+        $this->mock(KeyStorage::class, function ($mock) {
+            $mock->shouldReceive('storeKey');
+        });
+
+        $this->mock(ScriptsStorage::class, $scriptsStorageMock ?? function ($mock) {
+            $mock->shouldReceive('storeScript')->once()->andReturn(base_path('tests/fixtures/Ssh/script'));
+        });
+
+        $executor = m::mock(ProcessExecutor::class);
+        $executor->shouldReceive('run')->andReturn(new Response(0, 'success'));
+
+        return new ExecutorService($executor);
+    }
+
 }
