3caad47035ab96a9eb88c0802586093bf8848eb6
Pavel Buchnev <butschster@gmail.com>
2019-09-05 14:30:14 +0300

    Добавлен перехват редиректов и передача информации по ним в API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,22 +4,20 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
-     *
      * @var array
      */
-    protected $dontReport = [
-        //
+    protected $dontReport = [//
     ];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
-     *
      * @var array
      */
     protected $dontFlash = [
@@ -30,7 +28,8 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param \Exception $exception
+     *
      * @return void
      */
     public function report(Exception $exception)
@@ -41,20 +40,45 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param \Illuminate\Http\Request $request
+     * @param \Exception $exception
+     *
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        $response = parent::render($request, $exception);
+
+        if ($request->expectsJson()) {
+            if ($response instanceof RedirectResponse) {
+                return $this->redirectJson($request, $response);
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * Convert redirect response into a JSON response
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param RedirectResponse $response
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function redirectJson($request, RedirectResponse $response)
+    {
+        return response()->json([
+            'url' => $response->getTargetUrl(),
+        ], $response->getStatusCode());
     }
 
     /**
      * Convert a validation exception into a JSON response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Validation\ValidationException  $exception
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Validation\ValidationException $exception
+     *
      * @return \Illuminate\Http\JsonResponse
      */
     protected function invalidJson($request, ValidationException $exception)
