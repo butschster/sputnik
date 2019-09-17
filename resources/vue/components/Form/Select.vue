f55d6c0921b28133829694ff68c2713cc36e5158
Pavel Buchnev <butschster@gmail.com>
2019-09-17 17:18:21 +0300

    Поправлена работа и поведение input select в соответсвтии с другими полями ввода.

--- resources/vue/components/Form/Select.vue
+++ resources/vue/components/Form/Select.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="form-group form-group-labeled" :class="{'is-invalid': httpErrors.has(name), 'is-required': required}">
-        <vSelect class="form-control" :options="options" :value="value" @input="setSelected" :reduce="option => option.value"  />
-        <label :for="name">{{ label }}</label>
+    <div class="form-group" :class="{'is-invalid': httpErrors.has(name), 'is-required': required}">
+        <vSelect class="form-control"
+                 :options="options"
+                 :value="value" @input="setSelected"
+                 :reduce="option => option.value"
+                 :label="label"
+        />
+        <label>{{ label }}</label>
 
         <slot></slot>
         <span class="invalid-feedback" role="alert" v-if="httpErrors.has(name)">
