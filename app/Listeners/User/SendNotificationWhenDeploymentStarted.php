f5859a814703df3eb5a033480fb085fae710b264
Pavel Buchnev <butschster@gmail.com>
2019-11-07 21:20:24 +0300

    Исправлена отправка уведомления при старте деплоя

--- app/Listeners/User/SendNotificationWhenDeploymentStarted.php
+++ app/Listeners/User/SendNotificationWhenDeploymentStarted.php
@@ -9,7 +9,7 @@ class SendNotificationWhenDeploymentStarted
     public function handle(Running $event)
     {
         $event->deployment->server->user->notify(
-
+            new \App\Notifications\Server\Deployment\Running($event->deployment)
         );
     }
 }
